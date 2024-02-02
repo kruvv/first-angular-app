f0efbe4ca2421189f2e11264854617454d5c7099
kruvv <kruvv@mail.ru>
2024-02-02 16:05:17 +0300

    Add staic view product

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
+import { IProduct } from './models/product';
+import { products as data } from './data/products';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'first-ng-app';
+
+  products: IProduct[] = data;
 }
