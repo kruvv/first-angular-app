0e2e4bf90d8e831d33ecb8102f31aabfd4a5c6e9
kruvv <kruvv@mail.ru>
2024-02-02 19:28:23 +0300

    Added a service for obtaining product data and an error handler

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable, tap } from 'rxjs';
 import { IProduct } from './models/product';
-import { products as data } from './data/products';
+import { ProductsService } from './services/product.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'first-ng-app';
 
-  products: IProduct[] = data;
+  products$: Observable<IProduct[]>;
+  // products: IProduct[] = [];
+  loading = false;
+  constructor(private productsService: ProductsService) { }
+  ngOnInit(): void {
+    this.loading = true;
+    this.products$ = this.productsService
+      .getAll()
+      .pipe(tap(() => (this.loading = false)));
+    // this.productsService.getAll().subscribe((products) => {
+    //   this.products = products;
+    //   this.loading = false;
+    // });
+  }
 }
